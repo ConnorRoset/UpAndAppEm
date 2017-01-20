9d73aff87702ab02c1c77cc977abfb198cd25056
ConnorRoset <croset.estem@gmail.com>
2017-01-20 14:58:08 -0600

    Helper Adapter classes added

---
+++ app/src/main/java/edu/ualr/cpsc4399/cbroset/upandappem/Messages/MessagesRVAdapter.java
@@ -0,0 +1,8 @@
+package edu.ualr.cpsc4399.cbroset.upandappem.Messages;
+
+/**
+ * Created by connorroset on 1/20/17.
+ */
+
+public class MessagesRVAdapter {
+}
---
+++ app/src/main/java/edu/ualr/cpsc4399/cbroset/upandappem/MyExercises/MyExercisesRVAdapter.java
@@ -0,0 +1,8 @@
+package edu.ualr.cpsc4399.cbroset.upandappem.MyExercises;
+
+/**
+ * Created by connorroset on 1/20/17.
+ */
+
+public class MyExercisesRVAdapter {
+}
